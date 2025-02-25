bd3b538ff2b5010735ff003f7aa71a95eab664e7
Denis M <mochalov.denis@list.ru>
2025-02-25 09:11:09 +0500

    Add project files.

---
+++ DU.ConsoleApp/DUProgram.cs
@@ -0,0 +1,86 @@
+﻿namespace DU.ConsoleApp
+{
+    internal static class DUProgram
+    {
+        private static void Main(string[] args)
+        {
+            DirectoryInfo parentDirectory = null!;
+
+            for (var i = 0; i < args.Length; i++)
+            {
+                var arg = args[i];
+
+                if (arg == "-b") Extensions.SizeInBytes = true;
+                else if (arg == "-d")
+                {
+                    parentDirectory = new DirectoryInfo(args[i + 1]);
+                    i++;
+                }
+            }
+
+            parentDirectory ??= new DirectoryInfo(Environment.CurrentDirectory);
+
+            if (!parentDirectory.Exists) throw new DirectoryNotFoundException(parentDirectory.FullName);
+
+            Console.Title = parentDirectory.FullName;
+            Console.WriteLine("[IE] - Directory or Sub dirs has inaccessible files or dirs");
+
+            var parentDirFiles = parentDirectory.EnumerateFiles()
+                .Select(f => new { f.Name, Size = f.Length })
+                .ToArray();
+
+            var parentDirFilesSize = parentDirFiles.Sum(f => f.Size);
+            parentDirFilesSize.SizeToConsole(".", false, false);
+
+            var parentDirChildDirsSize = 0L;
+            var parentAnyInaccessible = false;
+
+            var enumerationOptions = new EnumerationOptions
+            {
+                IgnoreInaccessible = true,
+                RecurseSubdirectories = true
+            };
+
+            var childDirectories = parentDirectory.EnumerateDirectories()
+                .Select(childDir =>
+                {
+                    var fsz = 0L;
+                    var hasInaccessible = false;
+
+                    try
+                    {
+                        fsz = childDir.EnumerateFiles("", SearchOption.AllDirectories).Sum(f => f.Length);
+                    }
+                    catch (UnauthorizedAccessException)
+                    {
+                        hasInaccessible = parentAnyInaccessible = true;
+                        fsz = childDir.EnumerateFiles("*", enumerationOptions).Sum(f => f.Length);
+                    }
+
+                    parentDirChildDirsSize += fsz;
+
+                    return new { childDir.Name, Size = fsz, HasInaccessible = hasInaccessible };
+                })
+                .ToArray();
+
+            parentDirChildDirsSize.SizeToConsole(".dirs", true, parentAnyInaccessible);
+            (parentDirFilesSize + parentDirChildDirsSize).SizeToConsole(".total", true, parentAnyInaccessible);
+
+            foreach (var dir in childDirectories.OrderByDescending(d => d.Size))
+            {
+                dir.Size.SizeToConsole(dir.Name, true, dir.HasInaccessible);
+            }
+
+            foreach (var file in parentDirFiles)
+            {
+                file.Size.SizeToConsole(file.Name, false, false);
+            }
+
+            if (args.Contains("-w"))
+            {
+                Console.WriteLine("Done");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ DU.ConsoleApp/Extensions.cs
@@ -0,0 +1,29 @@
+﻿namespace DU.ConsoleApp
+{
+    internal static class Extensions
+    {
+        public static bool SizeInBytes { get; set; }
+
+        public static void SizeToConsole(this long size, string name, bool isDir, bool hasInaccessibleEntries)
+        {
+            Console.Write($"{size.SizeToString(),7}\t");
+            var defaultColor = Console.ForegroundColor;
+            Console.ForegroundColor = isDir ? ConsoleColor.Green : ConsoleColor.Blue;
+            if (hasInaccessibleEntries) Console.Write("[IE]");
+            Console.WriteLine(name);
+            Console.ForegroundColor = defaultColor;
+        }
+
+        public static string SizeToString(this long size)
+        {
+            if (SizeInBytes) return $"{size}b";
+            var curSize = size / Math.Pow(2, 30);
+            if (curSize > 1) return $"{curSize:N1}gb";
+            curSize = size / Math.Pow(2, 20);
+            if (curSize > 1) return $"{curSize:N1}mb";
+            curSize = size / Math.Pow(2, 10);
+            if (curSize > 1) return $"{curSize:N1}kb";
+            return $"{size}b";
+        }
+    }
+}
